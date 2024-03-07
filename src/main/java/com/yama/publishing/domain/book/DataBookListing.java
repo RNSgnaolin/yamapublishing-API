10a61faf12d4f654aeca9534744aa74ca089285a
RNSgnaolin <137301909+RNSgnaolin@users.noreply.github.com>
2024-03-07 18:32:19 -0300

    Update DataBookListing.java
    Remove ID, unnecessary for listing record

--- src/main/java/com/yama/publishing/domain/book/DataBookListing.java
+++ src/main/java/com/yama/publishing/domain/book/DataBookListing.java
@@ -2,10 +2,10 @@ package com.yama.publishing.domain.book;
 
 import com.yama.publishing.domain.genre.Genre;
 
-public record DataBookListing(Long id, String title, String author, Integer pages, Integer price, Genre genre) {
+public record DataBookListing(String title, String author, Integer pages, Integer price, Genre genre) {
 
     public DataBookListing(Book book) {
-        this(book.getId(), book.getTitle(), book.getAuthor().getName(), book.getPages(), book.getPrice(), book.getGenre());
+        this(book.getTitle(), book.getAuthor().getName(), book.getPages(), book.getPrice(), book.getGenre());
     }
     
 }
